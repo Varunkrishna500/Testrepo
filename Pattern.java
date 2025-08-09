4c33ea57152bfe7a9fb493a7d6ab2863c68466ad
Varunkrishna500 <varunkrishna452@gmail.com>
2025-08-09 13:11:20 +0530

    Create Pattern.java
    Prints the Pattern for any input 'n'.

---
+++ Pattern.java
@@ -0,0 +1,23 @@
+//Prints the right angles triangle patter
+/*
+
+*
+**
+***
+****
+
+*/
+import java.uitl.*;
+public class Pattern{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int i = 1;i <= n;i++){
+      for(int j = 1;j <= i;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  sc.close();
+  }
+}
